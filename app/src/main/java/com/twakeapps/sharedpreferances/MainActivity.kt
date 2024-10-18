4fccddaaa38983f1ad4c275d704fed565dfd7905
Md. Mowahibur Rahman Twake <twake.edu@gmail.com>
2024-10-18 11:27:31 +0600

    Encripted Share Pref

--- app/src/main/java/com/twakeapps/sharedpreferances/MainActivity.kt
+++ app/src/main/java/com/twakeapps/sharedpreferances/MainActivity.kt
@@ -1,22 +1,73 @@
 package com.twakeapps.sharedpreferances
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import com.twakeapps.sharedpreferances.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity()
 {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var sharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState : Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val masterKey = MasterKey.Builder(this)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build()
+
+        sharedPref = EncryptedSharedPreferences.create(
+                this,
+                "secret_my_data",
+                masterKey,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                                                                 )
+
+
+
+        //sharedPref = getSharedPreferences("my_data",Context.MODE_PRIVATE)
+
+        listeners()
+
 
 
     } // onCreate
+
+    private fun listeners(){
+
+        binding.btnSave.setOnClickListener {
+           val mName = binding.etName.text.toString()
+            sharedPref.edit().putString("MY_NAME",mName).apply()
+
+        }
+
+        binding.btnRead.setOnClickListener {
+
+            val myName = sharedPref.getString("MY_NAME","")?:""
+            binding.tvResult.text = myName
+
+        }
+
+        binding.btnClear.setOnClickListener {
+
+            binding.etName.text?.clear()
+            binding.tvResult.text = ""
+
+            sharedPref.edit().clear().apply() // clear all data
+
+        }
+
+    }
+
+
 }
